e5a9c7cf8b521b9c50177e4b83c093ae20ff8e7f
OmranAbazid <omranalrashid@Administrators-iMac-6.local>
2017-10-25 10:12:43 +0300

    moved everthing to build directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require("path");
 module.exports = {
   entry: ["./src/app.js"],
   output: {
-    path: __dirname,
+    path: path.join(__dirname, 'build/'),
     filename: "app.min.js"
   },
   module: {
